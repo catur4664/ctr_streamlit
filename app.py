bab11ade373e8d6bbcf82ca64750d8a673a68923
catur4664 <62492476+catur4664@users.noreply.github.com>
2023-11-18 21:55:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-import streamlit as stif st.button('Press Me'):
-    st.write('You Pressed')
-else:
-    st.write('You Not Press')
+import os
+
+import altair as alt
+import numpy as np
+import pandas as pd
+import pydeck as pdk
+import streamlit as st
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="NYC Ridesharing Demo", page_icon=":taxi:")
